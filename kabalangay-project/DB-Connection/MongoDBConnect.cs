4cf6a151c943b242e06a6d0813d04f6d0cd9def1
cb-1130 <christinebayas@gmail.com>
2024-06-07 10:43:39 +0800

    added mongodb file

---
+++ kabalangay-project/DB-Connection/MongoDBConnect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kabalangay_project.DB_Connection
+{
+    internal class MongoDBConnect
+    {
+    }
+}
